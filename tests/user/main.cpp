13446860f8e0192cbeb439b40d60908aa600b75b
Neyromancer <dmitry.kormulev@yandex.ru>
2019-01-13 19:59:52 +0300

    Method for testing GetId added.

--- tests/user/main.cpp
+++ tests/user/main.cpp
@@ -7,6 +7,24 @@
 
 #include "user/user.h"
 
+//class UserTest : public ::testing::Test {
+// protected:
+//  void SetUp override {
+//    user.
+//  }
+//
+// User user_;
+//};
+
+TEST (UserClassTest, UserIdEvaluation) {
+  User user1("User1", "User1");
+  User user2("User2", "User2");
+  User user3("User3", "User3");
+  EXPECT_EQ(user1.GetId(), 1);
+  EXPECT_EQ(user2.GetId(), 2);
+  EXPECT_EQ(user3.GetId(), 3);
+}
+
 TEST (UserClassTest, ConnectionSizeEvaluation) {
   User user("Umer", "Mansoor");
   EXPECT_EQ(user.GetNumberOfConnections(), 0);
@@ -22,6 +40,8 @@ TEST (UserClassTest, ConnectionSizeEvaluation) {
   EXPECT_EQ(user.GetNumberOfConnections(), 5);
   user.RemoveConnection(30);
   EXPECT_EQ(user.GetNumberOfConnections(), 4);
+  user.RemoveConnection(3);
+  EXPECT_EQ(user.GetNumberOfConnections(), 3);
 }
 
 int main(int argc, char **argv) {
